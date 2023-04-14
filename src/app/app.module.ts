1f800dc9f14e4dc338df9fcbeb808a8505e36661
FlorianIsenberg <florianisenberg@gmail.com>
2023-04-14 13:25:31 +0200

    fontPage

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FrontPageComponent } from './front-page/front-page.component';
+import { AboutMeComponent } from './about-me/about-me.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FrontPageComponent
+    FrontPageComponent,
+    AboutMeComponent
   ],
   imports: [
     BrowserModule,
