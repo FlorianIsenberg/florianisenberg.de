aacf74f04f0dcf61b411d688e40970b0f44ddfb9
FlorianIsenberg <florianisenberg@gmail.com>
2023-04-21 16:13:44 +0200

    contactField

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
@@ -18,13 +18,10 @@ import { ContactComponent } from './contact/contact.component';
     AboutMeComponent,
     SkillsComponent,
     PortfolioComponent,
-    ContactComponent
+    ContactComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,10 +1,66 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
-  styleUrls: ['./contact.component.scss']
+  styleUrls: ['./contact.component.scss'],
 })
-export class ContactComponent {
+export class ContactComponent implements OnInit {
+  isSubmitted = false;
+  emailSent: boolean = false;
 
+  myForm = new FormGroup({
+    name: new FormControl('', [Validators.required]),
+    email: new FormControl('', [Validators.required, Validators.email]),
+    message: new FormControl('', [
+      Validators.required,
+      Validators.minLength(10),
+    ]),
+  });
+
+  constructor(private http: HttpClient) {}
+  post = {
+    endPoint: 'PFAD ZUR PHP DATEI',
+    body: (payload: any) => JSON.stringify(payload),
+    options: {
+      headers: {
+        'Content-Type': 'text/plain',
+        responseType: 'text',
+      },
+    },
+  };
+
+  ngOnInit(): void {}
+
+  onSubmit(ngForm: any) {
+    this.isSubmitted = true;
+    if (this.myForm.valid) {
+      this.isSubmitted = false;
+      this.myForm.reset();
+    }
+    if (this.myForm.valid) {
+      let data = {
+        name: this.myForm.value.name,
+        email: this.myForm.value.email,
+        message: this.myForm.value.message,
+      };
+      this.http.post(this.post.endPoint, data).subscribe({
+        next: (response) => {
+          ngForm.resetForm();
+          this.emailSent = true;
+          this.timeOutSendMail();
+        },
+        error: (error) => {},
+      });
+    } else {
+    }
+  }
+
+  timeOutSendMail() {
+    setTimeout(() => {
+      this.emailSent = false;
+    }, 2000);
+  }
 }
